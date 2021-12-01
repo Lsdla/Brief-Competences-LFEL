532c66d87942154bd94b57081f3887bcd0847ba3
lobna4 <kharratlobna1@gmail.com>
2021-12-01 14:38:54 +0100

    page liste + fonctionnaliés

---
+++ app-arbre-competence/script/list.js
@@ -0,0 +1,79 @@
+import { User } from "./user";
+
+
+let list = "http://localhost:3000/users";
+let searchForm = document.querySelector('#search');   
+let apprenant = document.querySelector('.apprenants');
+
+function apprenantList(){
+
+    fetch(list)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+    })
+    .then(function (users) {
+
+        let tabUser = users;
+
+        for (let i = 0; i < tabUser.length; i++) {
+            let app = tabUser[i];
+
+           // console.log(app);
+
+            apprenant.innerHTML += `<div class=" p-3"> 
+                                         <div>
+                                      <a href="/template/progression.html?id=${app.id}">
+                                          ${app.lastname} ${app.fisrtname} 
+                                      </a>
+                                          </div>
+                                     </div>`;
+        }
+    });  
+         
+    }
+
+    let input = document.querySelector('#search');
+    input.addEventListener('change', filter)
+
+function filter(e){
+
+    apprenant.innerHTML='';
+
+    let valeur = e.target.value;
+    let uri = "http://localhost:3000/users?lastname="+ valeur;
+console.log(uri)
+    fetch(uri)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function (user) {
+            let tabUser = user;
+
+
+        for (let i = 0; i < tabUser.length; i++) {
+            let app = tabUser[i];
+
+           // console.log(app);
+
+            apprenant.innerHTML += `<div class=" p-3"> 
+                                      <div>
+                                      <a href="/template/progression.html?id=${app.id}">
+                                      ${app.lastname} ${app.fisrtname} 
+                                      </a>
+                                      </div>
+                                    </div>`;
+        }
+        if ( apprenant.innerHTML=='') {
+            apprenantList();
+        }
+    })
+}
+    apprenantList();
+
+
+
+

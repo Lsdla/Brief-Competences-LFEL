140c09d2e3057ab547d3ef7f179e7cc4d54f0644
Lsdla <al3s2@yahoo.fr>
2021-12-01 12:52:55 +0100

    ajout de la page progress

--- app-arbre-competence/script/skill.js
+++ app-arbre-competence/script/skill.js
@@ -5,3 +5,16 @@ export class Skill {
         this._wording= data.wording
     }
 }
+
+
+
+/**
+ * get des compétences pour chaque user au clique sur chaque user on récupére son id qu'on met dans l'uri
+ * mise en place d'un html dynamique 
+ * template += `
+      <div class="skill">
+        <h2>${activity.id}</h2>
+        <p>${skill.wording}</p>
+        <a href="/details.html?id=${skill.id}">Read more</a>
+      </div>
+ */
